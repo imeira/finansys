ddf452bf99a4785a46624aa4a703baec7cf08c19
Igor Meira <meira.igor@gmail.com>
2019-11-04 18:31:06 -0300

    page-header component

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   @Input('page-title') pageTitle: string;
-  @Input('show-button') showButton: boolean = true;
+  @Input('show-button') showButton = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
